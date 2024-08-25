8543dbc6526bd9f4290d3d5f2b48bf9f50c7627f
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-08-24 21:02:16 -0300

    feat(project): Added a test and POST method
    It was added a test code and a POST
    methos of how the API works.

--- API/main.py
+++ API/main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Flask is installed and working!'
+
+@app.route('/phase', methods=['POST'])
+def phase():
+    data = request.get_json()
+    number = data['escolha']
+    
+    if number == 1:
+        return 'Phase 1'
+    elif number == 2:
+        return 'Phase 2'
+    elif number == 3:
+        return 'Phase 3'
+    else:
+        return 'Escolha invalida'
 
 if __name__ == '__main__':
     app.run()
