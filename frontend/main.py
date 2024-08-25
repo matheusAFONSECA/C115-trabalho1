d6dd769b5f6f9dea4f9385e968d54744e611568b
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-08-24 21:23:15 -0300

    feat(project): buttons options added
    Two buttons that shows
    options to the user was
    added.

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,20 @@
 import streamlit as st
 
 def main():
-    st.title("Meu Aplicativo Streamlit")
-    st.write("Bem-vindo ao meu aplicativo!")
+    st.title("Chatbot de telefonia")
+    st.write("Bem-vindo, eu sou um chatbot de telefonia. Como posso te ajudar?")
+    st.write("Escolha uma das opções abaixo:")
+
+    # Add a chatbox
+    # user_input = st.text_input("Digite sua mensagem")
+
+    # Add three buttons
+    if st.button("Telefone"):
+        st.write("Você escolheu a opção 1!")
+    if st.button("Internet"):
+        st.write("Você escolheu a opção 2!")
+    # if st.button("Opção 3"):
+    #     st.write("Você escolheu a opção 3!")
 
 if __name__ == '__main__':
     main()
